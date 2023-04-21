db363383a130f704c79c8cf04de105f8aac5eb2b
kodjo touglo <micheltouglo@gmail.com>
2023-04-21 16:42:31 +0000

    handle sqlError

--- domain/users/user_dao.go
+++ domain/users/user_dao.go
@@ -6,18 +6,19 @@ import (
 	"github.com/KodjoTouglo/bookstore_users/storage/postgres/users_db"
 	"github.com/KodjoTouglo/bookstore_users/utils/date_utils"
 	"github.com/KodjoTouglo/bookstore_users/utils/errors"
+	"github.com/lib/pq"
 	"strings"
 )
 
-const queryInsertUser = "INSERT INTO users(first_name, last_name, email, date_created) VALUES($1, $2, $3, $4) " +
-	"RETURNING id;"
-
-var usersDB = make(map[int64]*User)
+const (
+	queryInsertUser  = "INSERT INTO users(first_name, last_name, email, date_created) VALUES($1, $2, $3, $4) RETURNING id;"
+	queryGetUserById = "SELECT id, first_name, last_name, email, date_created FROM users WHERE id = $1"
+)
 
 func (user *User) Save() *errors.APIError {
 	stmt, err := users_db.Client.Prepare(queryInsertUser)
 	if err != nil {
-		return errors.InternalServerError(err.Error())
+		return errors.InternalServerError("", err.Error())
 	}
 	defer func(stmt *sql.Stmt) {
 		err := stmt.Close()
@@ -29,27 +30,39 @@ func (user *User) Save() *errors.APIError {
 	var userId int64
 	err = stmt.QueryRow(user.FirstName, user.LastName, user.Email, user.DateCreated).Scan(&userId)
 	if err != nil {
-		if strings.Contains(err.Error(), "users_email_key") {
-			return errors.BadRequestError(fmt.Sprintf("email `%s` already exist", user.Email))
+		sqlErr, ok := err.(*pq.Error)
+		if ok {
+			switch sqlErr.Code {
+			case "23505":
+				return errors.BadRequestError(fmt.Sprintf("email `%s` already exist", user.Email))
+			default:
+				return errors.InternalServerError("error executing SQL statement", sqlErr.Error())
+			}
+		} else {
+			return errors.InternalServerError(err.Error(), "error creating user")
 		}
-		return errors.InternalServerError(err.Error())
 	}
 	user.Id = userId
 	return nil
 }
 
 func (user *User) Get() *errors.APIError {
-	if err := users_db.Client.Ping(); err != nil {
-		panic(err)
+	stmt, err := users_db.Client.Prepare(queryGetUserById)
+	if err != nil {
+		return errors.InternalServerError(err.Error())
 	}
-	result := usersDB[user.Id]
-	if result == nil {
-		return errors.NotFoundError(fmt.Sprintf("User %d not found", user.Id))
+	defer func(stmt *sql.Stmt) {
+		err := stmt.Close()
+		if err != nil {
+
+		}
+	}(stmt)
+	result := stmt.QueryRow(user.Id)
+	if err := result.Scan(&user.Id, &user.FirstName, &user.LastName, &user.Email, &user.DateCreated); err != nil {
+		if strings.Contains(err.Error(), "no rows in result set") {
+			return errors.NotFoundError(fmt.Sprintf("User `%d` not found", user.Id), err.Error())
+		}
+		return errors.InternalServerError(fmt.Sprintf("error when trying to get user `%d`", user.Id), err.Error())
 	}
-	user.Id = result.Id
-	user.FirstName = result.FirstName
-	user.LastName = result.LastName
-	user.Email = result.Email
-	user.DateCreated = result.DateCreated
 	return nil
 }
--- utils/errors/api_error.go
+++ utils/errors/api_error.go
@@ -3,27 +3,40 @@ package errors
 import "net/http"
 
 type APIError struct {
+	Message    string `json:"message"`
 	Detail     string `json:"detail"`
 	StatusCode int    `json:"status_code"`
 }
 
-func BadRequestError(detail string) *APIError {
-	return &APIError{
+func BadRequestError(detail string, message ...string) *APIError {
+	apiErr := &APIError{
 		Detail:     detail,
 		StatusCode: http.StatusBadRequest,
 	}
+	if len(message) > 0 {
+		apiErr.Message = message[0]
+	}
+	return apiErr
 }
 
-func NotFoundError(detail string) *APIError {
-	return &APIError{
+func NotFoundError(detail string, message ...string) *APIError {
+	apiErr := &APIError{
 		Detail:     detail,
 		StatusCode: http.StatusNotFound,
 	}
+	if len(message) > 0 {
+		apiErr.Message = message[0]
+	}
+	return apiErr
 }
 
-func InternalServerError(detail string) *APIError {
-	return &APIError{
+func InternalServerError(detail string, message ...string) *APIError {
+	apiErr := &APIError{
 		Detail:     detail,
 		StatusCode: http.StatusInternalServerError,
 	}
+	if len(message) > 0 {
+		apiErr.Message = message[0]
+	}
+	return apiErr
 }
